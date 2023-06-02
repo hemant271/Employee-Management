f217c96ad1ea8d35bda093c91e5a830e68d54f6c
Hemant Patidar <patidarhemant27@gmail.com>
2023-06-03 03:03:52 +0530

    Added dummy employee and updated routing.

--- FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit{
+    employees: Employee[] = [
+      {
+        id: '1',
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        phone: 11111,
+        salary: 60000,
+        department: 'HR'
+      } ,
+      {
+        id: '2',
+        name: 'Hemant Patidar',
+        email: 'hpatidar@livegage.ai',
+        phone: 11111,
+        salary: 50000,
+        department: 'IT'
+      }
 
+    ]
+    constructor() {    }
+
+    ngOnInit(): void {    }
 }
