74694bcb54bcc3b952ee04dcc9db81d0866ae305
Hemant Patidar <patidarhemant27@gmail.com>
2023-06-06 23:20:05 +0530

    FINAL commit - CRUD complete

--- FullStack UI/FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
+++ FullStack UI/FullStack.UI/src/app/components/employees/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Employee } from 'src/app/models/employee.model';
 import { EmployeesService } from 'src/app/services/employees.service';
 
@@ -18,7 +19,7 @@ export class AddEmployeeComponent implements OnInit {
     department: ''
   };
 
-  constructor(private employeeService: EmployeesService) {}
+  constructor(private employeeService: EmployeesService, private router: Router) {}
 
   ngOnInit(): void {
   }
@@ -28,7 +29,8 @@ export class AddEmployeeComponent implements OnInit {
     this.employeeService.addEmployee(this.addEmployeeRequest)
         .subscribe({
           next: (employee) => {
-            console.log(employee);
+            //console.log(employee);
+            this.router.navigate(['employees'])
           }
           
           
